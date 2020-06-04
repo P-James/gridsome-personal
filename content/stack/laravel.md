04e86acb20c1581a0b2bc88349dc77e8d5b83a8b
Perry James <perrydjames28@gmail.com>
2020-06-04 13:47:00 +0100

    update video

--- content/stack/laravel.md
+++ content/stack/laravel.md
@@ -12,8 +12,5 @@ in with Laravel that backend structure and OOP began to click.
 You can see an example of my most recent commercial work with Laravel at:
 [Educhina.co.uk](https://educhina.co.uk)
 
-Check out my [Chinese character search site](https://chinesecharactersearch.com) built with Laravel. Chinese characters are composed of a 'radical' and a component, but they're not always easily identifiable. Try searching any characters to see its radicals, eg 雇佣我.
-
-
 I'm also familiar with Wordpress and have built sites such as
 [LipleyAndParker.com](http://lipleyandparker.com) using that as a backend.
--- src/pages/Laravel.vue
+++ src/pages/Laravel.vue
@@ -1,6 +1,17 @@
 <template>
   <Layout>
     <div class="mx-2 flex flex-col justify-between" v-html="$page.stackType.content"></div>
+    <template v-slot:outsideFlex>
+      <hr />
+      <div class="flex flex-center flex-wrap">
+        <p class="mt-4">
+          Check out my
+          <a href="https://chinesecharactersearch.com">Chinese character search site</a> built with Laravel. Chinese characters are composed of a 'radical' and a component, but they're not always easily identifiable. Try searching any characters to see its radicals, eg 雇佣我.
+          I webscraped all the characters and radicals from a dictionary website and used <a href="https://laravel-livewire.com">Laravel Livewire</a> to query the database. I plan on scraping the components and adding compatibility for traditional Chinese characters.
+        </p>
+        <video class="mt-2" controls src="@/assets/ChineseCharacterSearch.webm" autoplay></video>
+      </div>
+    </template>
   </Layout>
 </template>
 
