48144be187da361fe507e875342614b0a2c2bd47
Perry James <perrydjames28@gmail.com>
2020-06-22 09:53:34 +0100

    tailwind project added

--- src/pages/Tailwind.vue
+++ src/pages/Tailwind.vue
@@ -1,6 +1,16 @@
 <template>
   <Layout>
     <div class="mx-2 flex flex-col justify-between" v-html="$page.stackType.content"></div>
+    <template v-slot:outsideFlex>
+      <hr />
+      <div class="flex flex-center flex-wrap">
+        <p class="mt-4">
+          Here's a WIP project I'm working on with Tailwind. The search returns stylised cards. The search feature is built with Laravel Livewire and the filter is made with AlpineJs. 
+          It searches through company names and their addresses. The database is seeded with 500 fake companies, and I'd like to improve performance. So if you spotted the Laravel debugbar in the bottom left, that's because I'm currently (June 2020) learning about optimising SQL queries with Eloquent.
+        </p>
+        <video class="mt-2" controls src="@/assets/fishery-search.webm" autoplay></video>
+      </div>
+    </template>
   </Layout>
 </template>
 
