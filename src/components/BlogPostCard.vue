322f65c37f2544eec937d901dd4069019e15a713
Perry James <perrydjames28@gmail.com>
2020-07-12 12:16:28 +0100

    card hover styling

--- src/components/BlogPostCard.vue
+++ src/components/BlogPostCard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="leading-tight">
-    <h2><a 
-    :href="slug"
-    class="font-semibold leading-tight hover:underline cursor-pointer">{{title}}</a></h2>
-    <slot></slot>
-    <div class="italic flex justify-end mt-4 saturated">Posted: {{date}}</div>
-  </div>
+  <a :href="slug" class="blog-post-card">
+    <div class="leading-tight">
+      <h2 class="mb-2">
+        <a :href="slug" class="font-semibold leading-tight cursor-pointer">{{title}}</a>
+      </h2>
+      <slot></slot>
+      <div class="italic flex justify-end mt-4 saturated">Posted: {{date}}</div>
+    </div>
+  </a>
 </template>
 
 <script>
@@ -15,4 +17,7 @@ export default {
 </script>
 
 <style>
+.blog-post-card:hover a {
+  @apply underline;
+}
 </style>
