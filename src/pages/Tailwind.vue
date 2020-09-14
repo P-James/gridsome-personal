3b4718dbc1e8e6f7a2cc933385791622e22fb150
Perry James <perrydjames28@gmail.com>
2020-09-14 15:58:18 +0100

    update

--- src/pages/Tailwind.vue
+++ src/pages/Tailwind.vue
@@ -6,7 +6,7 @@
       <div class="flex flex-center flex-wrap">
         <p class="mt-4">
           Here's a WIP project I'm working on with Tailwind. The search returns stylised cards. The search feature is built with Laravel Livewire and the filter is made with AlpineJs. 
-          It searches through company names and their addresses. The database is seeded with 500 fake companies, and I'd like to improve performance. So if you spotted the Laravel debugbar in the bottom left, that's because I'm currently (June 2020) learning about optimising SQL queries with Eloquent.
+          It searches through company names and their addresses. The database is seeded with 500 fake companies, and I'd like to improve performance. So if you spotted the Laravel debugbar in the bottom left, that's because I was learning about optimising SQL queries with Eloquent.
         </p>
         <video class="mt-2" controls src="@/assets/fishery-search.webm" autoplay></video>
       </div>
